a3c76e2db5ecb7f90357e32d73e9dcf564d8d9b1
watercore <watercore2001@gmail.com>
2023-10-14 15:17:32 +0800

    first commit

---
+++ src/preprocessing/main.py
@@ -0,0 +1,23 @@
+import os.path
+from preprocessing.util import get_last_level_sub_folders, stack_bands_and_crop, copy_metadata
+import argparse
+
+
+def preprocess1(input_root_folder: str, output_root_folder: str):
+    for rel_path in get_last_level_sub_folders(input_root_folder):
+        input_folder = os.path.join(input_root_folder, rel_path)
+        output_folder = os.path.join(output_root_folder, rel_path)
+        stack_bands_and_crop(input_folder, output_folder, dst_resolution=10, window_size=512, overlap_size=64)
+        copy_metadata(input_folder, output_folder)
+
+def test_preprocess1():
+    input_root_folder = r"C:\Users\watercore\Desktop\1"
+    output_root_folder = r"C:\Users\watercore\Desktop\2"
+    preprocess1(input_root_folder, output_root_folder)
+
+def preprocess1_cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_folder", type=str)
+    parser.add_argument("-o", "--output_folder", type=str)
+    args = parser.parse_args()
+    preprocess1(args.input_folder, args.output_folder)
---
+++ src/preprocessing/util.py
@@ -0,0 +1,127 @@
+import rasterio
+from rasterio.windows import Window
+from rasterio.enums import Resampling
+import numpy as np
+import shutil
+import os
+
+def copy_metadata(input_folder: str, output_folder: str):
+    os.makedirs(output_folder, exist_ok=True)
+    copy_filenames = ["tileinfo_metadata.json", "granule_metadata.xml"]
+    for filename in copy_filenames:
+        input_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+        shutil.copy2(input_path, output_path)
+
+
+def read_data_with_up_sample(input_path: str, dst_resolution: int):
+    with rasterio.open(input_path) as src:
+        src_resolution = max(src.res)
+        if src_resolution == dst_resolution:
+            return src.read(), src.profile
+        else:
+            upscale_factor = int(src_resolution / dst_resolution)
+            dst_data = src.read(
+                out_shape=(
+                    src.count,
+                    src.height * upscale_factor,
+                    src.width * upscale_factor
+                ),
+                resampling=Resampling.bilinear)
+            dst_transform = src.transform * src.transform.scale((1 / upscale_factor), (1 / upscale_factor))
+            profile = src.profile
+            profile.update(transform=dst_transform,
+                           width=dst_data.shape[1],
+                           height=dst_data.shape[2],
+                           blockxsize=profile["blockxsize"]*upscale_factor,
+                           blockysize=profile["blockysize"]*upscale_factor)
+            return dst_data, profile
+
+
+def up_sample_and_save_as_tif(input_path: str, output_path: str, dst_resolution:int):
+    data, profile = read_data_with_up_sample(input_path, dst_resolution)
+
+    with rasterio.open(output_path, 'w', **profile) as dst:
+        dst.write(data)
+
+
+def up_sample_and_sqrt_and_save_as_jpeg(input_path: str, output_path: str, dst_resolution:int):
+    data, profile = read_data_with_up_sample(input_path, dst_resolution)
+
+    # Apply square root operation
+    sqrt_data = np.sqrt(data).astype(np.uint8)
+
+    profile.update(driver="JPEG", dtype=sqrt_data.dtype)
+
+    # Save as an 8-bit JPEG
+    with rasterio.open(output_path, 'w', **profile) as dst:
+        dst.write(sqrt_data)
+
+
+def stack_bands_and_crop(input_folder: str, output_folder: str,  dst_resolution: int, window_size: int, overlap_size: int):
+    input_filenames = ["B02.tif", "B03.tif", "B04.tif", "B05.tif", "B06.tif",
+                       "B07.tif", "B08.tif", "B8A.tif", "B11.tif", "B12.tif"]
+
+    # read data and profile
+    data = None
+    profile = None
+    for input_filename in input_filenames:
+        input_path = os.path.join(input_folder, input_filename)
+        temp_data, temp_profile = read_data_with_up_sample(input_path, dst_resolution=dst_resolution)
+        if data is None and profile is None:
+            data = temp_data
+            profile = temp_profile
+        else:
+            data = np.concatenate([data, temp_data], axis=0)
+            profile["count"] += 1
+
+    input_path_for_src = os.path.join(input_folder, input_filenames[0])
+    with rasterio.open(input_path_for_src) as src:
+        window_transform = src.window_transform
+
+    os.makedirs(output_folder, exist_ok=True)
+    real_window_size = window_size - overlap_size
+    row_count = (profile["height"] - window_size) // real_window_size + 1
+    col_count = (profile["width"] - window_size) // real_window_size + 1
+
+    for row_id in range(row_count):
+        row_start = row_id * real_window_size
+        row_end = row_start + window_size
+        for col_id in range(col_count):
+            col_start = col_id * real_window_size
+            col_end = col_start + window_size
+
+            window = Window.from_slices(slice(row_start, row_end), slice(col_start, col_end))
+            dst_transform = window_transform(window)
+            profile.update(width=window_size,
+                           height=window_size,
+                           transform=dst_transform)
+            dst_data = data[:, row_start:row_end, col_start:col_end]
+
+            output_filename = f"{row_id+1:02}{col_id+1:02}.tif"
+            output_path = os.path.join(output_folder, output_filename)
+
+            with rasterio.open(output_path, "w", **profile) as dst:
+                dst.write(dst_data)
+
+def test_stack_bands_and_crop():
+    input_folder = r"C:\Users\watercore\Desktop\1\S2A_43SCC_20221219_0_L2A"
+    output_folder = r"C:\Users\watercore\Desktop\1\output"
+    dst_resolution = 10
+    window_size = 512
+    overlap_size = 64
+    stack_bands_and_crop(input_folder, output_folder, dst_resolution, window_size, overlap_size)
+
+def get_last_level_sub_folders(root_folder: str):
+    for dir_path, dirs, files in os.walk(root_folder):
+        if len(dirs) == 0:
+            yield os.path.relpath(dir_path, root_folder)
+
+
+
+
+
+
+
+
+
